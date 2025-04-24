5766f40428a83b024417a57040bbaff63a53f1ca
SigPresidente <andrea.poletti.94@gmail.com>
2025-04-24 06:55:17 +0200

    added possibility to work with other books

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from stats import word_counting
 from stats import characters_counting
 from stats import order_by_count
 from stats import filter
+import sys
+from sys import argv
 
 #Function to transform a .txt in a long string
 def get_book_text (file_path) :
@@ -11,7 +13,12 @@ def get_book_text (file_path) :
     return path_as_string
 
 #MAIN
-link = "books/frankenstein.txt"
+if (len(sys.argv) == 2) :
+    link = sys.argv[1]
+else :
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 book = get_book_text(link) #Book is a long string of the full book
 words = book.split() #Renders the book string in a list of word elements
 number_of_words = word_counting(words) #Counts the number of elements of words list
